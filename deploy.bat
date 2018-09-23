6814198f6f6ca2f48517bedcb2967a6486acc56b
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-09-23 06:59:56 -0400

    More updates and cleanup by forum users @Gordon Dry and @banditsan

--- deploy.bat
+++ deploy.bat
@@ -4,6 +4,7 @@ rem @echo off
 set H=R:\KSP_1.4.3_dev
 set GAMEDIR=000_FilterExtensions
 set GAMEDIR2="000_FilterExtensions_Configs"
+set GAMEDIR3="zFinal_FilterExtensions"
 set GAMEDATA="GameData\"
 set VERSIONFILE=FilterExtension.version
 
@@ -16,3 +17,4 @@ copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 xcopy  /y /s /I  %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
 
 xcopy  /y /s /I   %GAMEDATA%\%GAMEDIR2% "%H%\GameData\%GAMEDIR2%"
+xcopy  /y /s /I   %GAMEDATA%\%GAMEDIR3% "%H%\GameData\%GAMEDIR3%"
