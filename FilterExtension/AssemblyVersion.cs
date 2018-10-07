a24f69bfa750d25196a79caa2b5b1939a0d63438
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-10-07 16:03:54 -0400

    Update changelog Update versin

--- FilterExtension/AssemblyVersion.cs
+++ FilterExtension/AssemblyVersion.cs
@@ -10,4 +10,4 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("3.2.2.8")]
+ [assembly: AssemblyVersion("3.2.2.9")]
--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 
 @echo off
 
-set H=R:\KSP_1.4.3_dev
+set H=R:\KSP_1.4.5_dev
 set GAMEDIR=000_FilterExtensions
 set GAMEDIR2="000_FilterExtensions_Configs"
 set GAMEDIR3="zFinal_FilterExtensions"
